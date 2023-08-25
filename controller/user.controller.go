eefc3f7fd6af11b260ff6064d4c1e1d3d1ad3042
jumairahmad <jumairahmad@gmail.com>
2023-08-26 02:38:02 +0500

    3rd day

--- controller/user.controller.go
+++ controller/user.controller.go
@@ -1,7 +1,10 @@
 package controller
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"restapi.com/my-apis/models"
 	"restapi.com/my-apis/services"
 )
 
@@ -17,24 +20,69 @@ func New(userservice services.UserServices) UserController {
 
 func (uc *UserController) CreateUser(ctx *gin.Context) {
 
-	ctx.JSON(200, "")
+	var user models.User
+
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	err := uc.UserServices.CreateUser(&user)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "Success"})
 }
 
 func (uc *UserController) GetUsers(ctx *gin.Context) {
+	username := ctx.Param("name")
+	user, err := uc.UserServices.GetUsers(&username)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
 
-	ctx.JSON(200, "")
 }
 
 func (uc *UserController) GetAll(ctx *gin.Context) {
-	ctx.JSON(200, "")
+	users, err := uc.UserServices.GetAll()
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, users)
+
 }
 
 func (uc *UserController) UpdateUser(ctx *gin.Context) {
-	ctx.JSON(200, "")
+
+	var user models.User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	err := uc.UserServices.UpdateUser(&user)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "Success"})
+
 }
 
 func (uc *UserController) DeleteUser(ctx *gin.Context) {
-	ctx.JSON(200, "")
+	username := ctx.Param("name")
+	err := uc.UserServices.DeleteUser(&username)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "Success"})
 }
 
 func (uc *UserController) RegisterUserRoutes(rg *gin.RouterGroup) {
@@ -44,6 +92,6 @@ func (uc *UserController) RegisterUserRoutes(rg *gin.RouterGroup) {
 	userroute.GET("/get/:name", uc.GetUsers)
 	userroute.GET("/getall", uc.GetAll)
 	userroute.PATCH("/update", uc.UpdateUser)
-	userroute.DELETE("/delete", uc.DeleteUser)
+	userroute.DELETE("/delete/:name", uc.DeleteUser)
 
 }
--- services/user_services_impl.go
+++ services/user_services_impl.go
@@ -2,7 +2,9 @@ package services
 
 import (
 	"context"
+	"errors"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"restapi.com/my-apis/models"
 )
@@ -22,22 +24,39 @@ func NewUserService(usercollection *mongo.Collection, ctx context.Context) UserS
 
 func (u *userServiceImpl) CreateUser(user *models.User) error {
 
-	return nil
+	_, err := u.usercollection.InsertOne(u.ctx, u)
+	return err
 }
 
 func (u *userServiceImpl) GetUsers(name *string) (*models.User, error) {
 
-	return nil, nil
+	var user *models.User
+	query := bson.D{bson.E{Key: "user_name", Value: name}}
+	err := u.usercollection.FindOne(u.ctx, query).Decode(&user)
+
+	return user, err
 }
 
 func (u *userServiceImpl) GetAll() ([]*models.User, error) {
 	return nil, nil
 }
 
-func (u *userServiceImpl) UpdateUser(*models.User) error {
+func (u *userServiceImpl) UpdateUser(user *models.User) error {
+	filter := bson.D{bson.E{Key: "user_name", Value: user}}
+	update := bson.D{bson.E{Key: "$set", Value: bson.D{bson.E{Key: "user_name", Value: user.Name}, bson.E{Key: "user_age", Value: user.Age}, bson.E{Key: "user_address", Value: user.Address}}}}
+
+	result, _ := u.usercollection.UpdateOne(u.ctx, filter, update)
+	if result.MatchedCount != 1 {
+		return errors.New("No Matched Found For Update")
+	}
 	return nil
 }
 
 func (u *userServiceImpl) DeleteUser(name *string) error {
+	filter := bson.D{bson.E{Key: "user_name", Value: &name}}
+	result, _ := u.usercollection.DeleteOne(u.ctx, filter)
+	if result.DeletedCount != 1 {
+		return errors.New("No Matched Found For Delete")
+	}
 	return nil
 }
